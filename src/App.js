894e727896da07944af4ee73f63429b2aa90d43e
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2020-07-28 12:25:50 -0400

    navigation complete with styling and gitlab icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
+import Navigation from "./components/Navigation";
+import AboutMe from './components/AboutMe';
 
-const App = ()=>{
-    return(
-        <div>Hey</div>
-    )
-}
+const App = () => {
+  return (
+    <div className="">
+        <Navigation />
+        <AboutMe />
+    </div>
+  );
+};
 
 export default App;
