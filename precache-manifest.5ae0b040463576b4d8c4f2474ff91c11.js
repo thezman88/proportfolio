f821a988ea3997b241caf539658a1810951fbbcd
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2020-07-28 23:50:10 -0400

    Updates

---
+++ precache-manifest.5ae0b040463576b4d8c4f2474ff91c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783da4fa0df8ae7c452d3441d7dbd45a",
+    "url": "/proportfolio/index.html"
+  },
+  {
+    "revision": "a8d83c8d1e8010f0faef",
+    "url": "/proportfolio/static/css/main.f09e9f43.chunk.css"
+  },
+  {
+    "revision": "7235dc09cad9da8b17bc",
+    "url": "/proportfolio/static/js/2.c5f420ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proportfolio/static/js/2.c5f420ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d83c8d1e8010f0faef",
+    "url": "/proportfolio/static/js/main.3cf18516.chunk.js"
+  },
+  {
+    "revision": "a9f8e0c21c1635087534",
+    "url": "/proportfolio/static/js/runtime-main.500f1b84.js"
+  }
+]);
